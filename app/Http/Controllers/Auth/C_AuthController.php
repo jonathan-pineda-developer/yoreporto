6cf67a6dfc83ab8ad71c6d9bf5c0050e91310bb0
Victor Segura <seguravictor57@gmail.com>
2022-10-07 15:34:54 -0600

    metodo login

--- app/Http/Controllers/Auth/C_AuthController.php
+++ app/Http/Controllers/Auth/C_AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class C_AuthController extends Controller
@@ -27,7 +29,7 @@ class C_AuthController extends Controller
             'nombre' => $request->nombre,
             'apellidos' => $request->apellidos,
             'correo' => $request->correo,
-            'password' => hash('sha256', $request->password)
+            'password' => Hash::make($request->password)
         ]);
 
         // token
@@ -39,5 +41,34 @@ class C_AuthController extends Controller
             'user' => $user,
             'token' => $token // retornamos el token
         ], 201);
+    }
+
+
+    public function login(Request $request)
+    {
+        // validacion del request
+        $this->validate($request, [
+            'correo' => 'required|email',
+            'password' => 'required|string|min:6|max:16',
+        ]);
+
+        // credenciales
+        $credenciales = $request->only('correo', 'password');
+
+        try {
+            // si no existe el token para el usuario
+            if (!$token = JWTAuth::attempt($credenciales)) {
+                return response()->json([
+                    'message' => 'Credenciales incorrectas'
+                ], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'message' => 'No se pudo crear el token'
+            ], 500);
+        }
+
+        // si todo es correcto
+        return response()->json(compact('token'));
     }
 }
