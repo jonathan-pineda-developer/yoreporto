c70d5b444d2e555a63bb195574b95a4c03aad477
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2022-10-22 20:51:16 -0600

    mostrar, actualizar e inactivar de usuarios

--- app/Http/Controllers/C_UserController.php
+++ app/Http/Controllers/C_UserController.php
@@ -2,9 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class C_UserController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show()
+    {
+        if (User::all()->isEmpty()) {
+            return response()->json(['message' => 'No hay registros'], 404);
+        } else {
+            $str_user = User::all();
+            return response ($str_user,200);
+        }
+    }
+    public function edit(Request $str_request, $vint_id)
+    {
+
+        $str_user = User::find($vint_id);
+        if (User::find($vint_id) == null) {
+            return response()->json(['message' => 'No se encontro el registro'], 404);
+        } else {
+            $str_user->update($str_request->all());
+            return response($str_user, 200);
+        }
+    }
+      /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+
+    public function update(Request $str_request, $vint_id)
+    {
+        $str_campos=[
+            'nombre' => 'required|string|max:30',
+            'apellidos' => 'required|string|max:70',
+            'correo' => 'required|string|max:100|unique:users|email',
+            'password' => 'required|string|max:100',
+        ];
+        return reponse ()->json([
+            'required' => 'El :attribute es requerido'], 404);
+
+        if ($str_request->hasFile('imagen')) {
+            $str_campos=['imagen'=>'required|max:10000|mimes:jpeg,png,jpg'];
+           
+        }
+
+        $this->validate($str_request,$str_campos,$str_mensaje);
+
+        $str_datosUser=request()->except(['_token','_method']);
+
+        if ($str_request->hasFile('imagen')) {
+            $str_user=User::findOrFail($vint_id);
+
+            Storage::delete('public/'.$str_user->imagen);
+
+            $str_datosUser['imagen']=$str_request->file('imagen')->store('uploads','public');
+        }
+
+        User::where('id','=',$vint_id)->update($str_datosUser);
+
+        $str_user=User::findOrFail($vint_id);
+
+         return response($str_user, 200);
+
+
+
+    }
+
+    public function inactivar(Request $str_request,$vint_id)
+    {
+        $str_user = User::find($vint_id);
+        if (User::find($vint_id) == null) {
+            return reponse()->json([
+                'message' => 'No se encontro el registro'], 404);
+        } else {
+            $str_user->estado = 0;
+            $str_user->save();
+            $str_user->update($str_request->all());
+            //retornar mensaje y datos del usuario
+            return response()->json([
+                'message' => 'Usuario inactivado correctamente'
+                
+            ], 200);
+        }
+    
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\C_AuthController;
+use App\Http\Controllers\C_UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,9 @@ use App\Http\Controllers\C_AuthController;
 Route::post('registro', [C_AuthController::class, 'registro']);
 Route::post('login', [C_AuthController::class, 'login']);
 
-
+Route::get('/mostrar_usuarios', [C_UserController::class, 'show']);
+Route::put('/actualizar/{id}', [C_UserController::class, 'edit']);
+Route::put('/inactivar/{id}', [C_UserController::class, 'inactivar']);
 // rutas protegidas
 
 Route::group(['middleware' => 'jwt.verify'], function () {
