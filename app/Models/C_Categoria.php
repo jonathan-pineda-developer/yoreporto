224adb0e768f8384d17d318c273a2c69e68c4c14
Victor Segura <seguravictor57@gmail.com>
2022-10-24 11:26:15 -0600

    relacion entre categoria y reporte

--- app/Models/C_Categoria.php
+++ app/Models/C_Categoria.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class C_Categoria extends Model
 {
     use HasFactory;
+
+    // tabla de la base de datos
+    protected $table = 'TB_Categoria';
+
+
+    // relacion con reporte de uno a muchos
+    public function reporte()
+    {
+        return $this->hasMany(C_Reporte::class);
+    }
 }
--- app/Models/C_Reporte.php
+++ app/Models/C_Reporte.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class C_Reporte extends Model
 {
     use HasFactory;
+
+    // tabla de la base de datos
+    protected $table = 'TB_Reporte';
+
+    // relacion con categoria de muchos a uno
+    public function categoria()
+    {
+        return $this->belongsTo(C_Categoria::class);
+    }
 }
