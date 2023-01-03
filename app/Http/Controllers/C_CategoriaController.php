5e85c31475faf999498c76aa20d1842372d180cd
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-01-03 11:55:12 -0600

    metodos de categoria

--- app/Http/Controllers/C_CategoriaController.php
+++ app/Http/Controllers/C_CategoriaController.php
@@ -8,100 +8,138 @@ use Validator;
 
 class C_CategoriaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $categorias = C_Categoria::all();
-        return $categorias;
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+   //metodo para crear una categoria
 
     public function store(Request $request)
     {
         $categoria = new C_Categoria();
-        $categoria->id = $request->id;
-        $categoria->descripcion = $request->descripcion; 
-        $categoria->user_id = $request->user_id;
+        $categoria->descripcion = $request->descripcion;
+    
+        $mensaje = [
+            'required' => 'El campo :attribute es requerido',
+            'descripcion.max' => 'El campo descripcion no debe ser mayor a 100 caracteres',
+        ];
+    
+        $this->validate($request, [
+            'descripcion' => 'required|string|max:100',
+        ], $mensaje);
+    
+        $categoria = C_Categoria::create(
+            [
+                'id' => $categoria->id,
+                'descripcion' => $categoria->descripcion,
+            ]
+        );
+    
+        //si la categoria se crea correctamente
 
-        request()->validate(C_Categoria::$rules);
-
-        $categoria->save();
+        if ($categoria) {
+            return response()->json([
+                
+                'message' => 'Categoria creada correctamente',
+                'categoria'=> $categoria
+               
+            ], 200);
+        } else {
+            return response()->json([
+              
+                'message' => 'Error al crear la categoria',
+             
+            ], 400);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    //metodo para mostrar todas las categorias
+
+    public function mostrar()
     {
-        //
+     //si hay categorias
+
+        if (C_Categoria::count() > 0) {
+            $categorias = C_Categoria::all();
+            return response()->json([
+                'categorias' => $categorias,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'No hay categorias registradas',
+            ], 400);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    //metodo para eliminar una categoria
+
+    public function destroy($id)
     {
-        //
-    }
+        //si se encuentra la categoria con el id
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+        if (C_Categoria::where('id', $id)->exists()) {
+        $categoria = C_Categoria::find($id);
+        $categoria->delete();
+        
+        if ($categoria) {
+            return response()->json([
+                'message' => 'Categoria eliminada correctamente',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Error al eliminar la categoria',
+            ], 400);
+        }
+    } else {
+        return response()->json([
+            'message' => 'No se encuentra la categoria',
+        ], 400);
+    }
+}
+
+    //metodo para actualizar una categoria
+
     public function update(Request $request, $id)
     {
-        $categoria = C_Categoria::findOrFail($id);
-        $categoria->descripcion = $request->descripcion; 
-        $categoria->user_id = $request->user_id;
+        //si se encuentra la categoria con el id
 
-        request()->validate(C_Categoria::$rules);
-
-        $categoria->save();
-
-        return $categoria;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-
+        if (C_Categoria::where('id', $id)->exists()) {
+            $categoria = C_Categoria::find($id);
+            $categoria->descripcion = $request->descripcion;
     
-    public function destroy(Request $request, $id)
-    {
-        $categoria = C_Categoria::destroy($request->$id);
-        return $categoria;
+            $mensaje = [
+                'required' => 'El campo :attribute es requerido',
+                'descripcion.max' => 'El campo descripcion no debe ser mayor a 100 caracteres',
+            ];
+    
+            $this->validate($request, [
+                'descripcion' => 'required|string|max:100',
+            ], $mensaje);
+    
+            $categoria = C_Categoria::where('id', $id)->update(
+                [
+                    'id' => $categoria->id,
+                    'descripcion' => $categoria->descripcion,
+                ]
+            );
+    
+            //si la categoria se actualiza correctamente
+    
+            if ($categoria) {
+                return response()->json([
+                    
+                    'message' => 'Categoria actualizada correctamente',
+            
+                   
+                ], 200);
+            } else {
+                return response()->json([
+                  
+                    'message' => 'Error al actualizar la categoria',
+                 
+                ], 400);
+            }
+        } else {
+            return response()->json([
+                'message' => 'No se encuentra la categoria',
+            ], 400);
+        }
     }
     
 }
