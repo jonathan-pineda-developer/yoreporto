00f4fe1d0e62ed8f62458c41b28f82ccc79666b0
Victor Segura <seguravictor57@gmail.com>
2023-01-05 17:06:52 -0600

    email de notificacion de nuevo UTE

--- resources/views/emails/notificacionRol/notificacionRolUTE.blade.php
+++ resources/views/emails/notificacionRol/notificacionRolUTE.blade.php
@@ -3,7 +3,7 @@
  <h1>Bienvenido al sistema Yo Reporto</h1>
  <p>Usted ha sido agregado al sistema con el rol: {{ $user->rol }}</p>
  <p>Para ingresar al sistema debe ingresar a la siguiente dirección: <a href="http://localhost:8000/login">http://localhost:8000/login</a></p>
-    <p>El usuario es: {{ $user->email }}</p>
+    <p>Su usuario es: {{ $user->email }}</p>
     <p>La contraseña es: {{ $password }}</p>
  </body>
 </html>
