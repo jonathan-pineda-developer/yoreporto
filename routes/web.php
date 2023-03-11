2e3752a4c240c2b240584736a6f2be406e668896
Mauren M <mau18.m@gmail.com>
2023-03-11 14:36:40 -0600

    renaming views and deleting test views

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('pleb/welcome_member', function () {
 Route::get('pleb/forgot_password', function () {
 
     $user = App\Models\User::find('98842033-952d-46f4-b0cf-6cbc5e42a7e7');
-    return new App\Mail\ForgotPassword($user);
+    return new App\Mail\RecuperacionContrasenia($user);
 });
 
 Route::get('pleb/reject_report', function () {
@@ -36,4 +36,6 @@ Route::get('pleb/reject_report', function () {
     $user = App\Models\User::find('98842033-952d-46f4-b0cf-6cbc5e42a7e7');
     return new App\Mail\RejectedReport($motivo, $reporte, $user, $ute);
 });
+
 */
+
