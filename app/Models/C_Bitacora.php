e794028b42b1de7b64a97f11195120d94bf4841b
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-03-13 13:43:11 -0600

    clase modelo de bitacora

---
+++ app/Models/C_Bitacora.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Eloquent\Model;
+
+class C_Categoria extends Model
+{
+    use HasFactory;
+
+    // tabla de la base de datos
+    protected $table = 'TB_Bitacora';
+
+    // llave primaria
+    protected $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'operation',
+        'ute',
+        'reporte_id',
+        'modified_at',
+    ];
+
+    // relacion con reporte de uno a muchos
+    public function reporte()
+    {
+        return $this->hasMany(C_Reporte::class);
+    }
+    // relacion con usuario de uno a muchos
+    /*
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne('App\Models\User', 'id', 'user_id');
+    }
+
+}
