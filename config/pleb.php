131c307aa648b7143ad0df3a074f446e77574867
Mauren M <mau18.m@gmail.com>
2023-03-25 17:19:17 -0600

    cambiando config->pleb

--- config/pleb.php
+++ config/pleb.php
@@ -1,29 +1,10 @@
 <?php
 
-/**
- * Putting this here to help remind you where this came from.
- *
- * I'll get back to improving this and adding more as time permits
- * if you need some help feel free to drop me a line.
- *
- * * Twenty-Years Experience
- * * PHP, JavaScript, Laravel, MySQL, Java, Python and so many more!
- *
- *
- * @author  Simple-Pleb <plebeian.tribune@protonmail.com>
- * @website https://www.simple-pleb.com
- * @source https://github.com/simplepleb/laravel-email-templates
- *
- * @license Free to do as you please
- *
- * @since 1.0
- *
- */
-
 return [
 
     'mail' => [
-        'top_logo'           => 'http://localhost:8000/assets/img/emails/yoReportoC.png', //'assets/img/emails/yoReporto.png',
+        'top_logo'           => 'https://drive.google.com/uc?id=19bxjX2HdehB_e5uOqR4CbBxQ1IgHHDiu', //'assets/img/emails/yoReporto.png',
+        'flecha_encabezado'  => 'https://drive.google.com/uc?id=1q8SSZSAd2cxNnv2auzCo5Lz9xNR2UXvy',
         'ingresar_url'       => 'http://localhost:8000/login',
         'direccion'          => '',
         'numero_telefono'    => '',
