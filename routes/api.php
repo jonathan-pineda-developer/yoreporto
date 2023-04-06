11e299b6d010210a9091c3fc5d2445df78d73c3b
Victor Segura <seguravictor57@gmail.com>
2023-04-06 15:26:40 -0600

    ruta para mostrar reportes Aceptados o Finalizados

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::post('actualizar_imagen/reportes/{id}', [C_ReporteController::class, 'upd
 Route::get('user_reportes', [C_ReporteController::class, 'showByUserId'])->middleware('jwt.verify');
 Route::get('reportes', [C_ReporteController::class, 'showAll']);
 Route::get('mostrar_reportes', [C_ReporteController::class, 'showAllReportes']);
+Route::get('aceptadosFinalidasos', [C_ReporteController::class, 'showReportesAceptadosoFinalizados']);
 Route::get('/reportes/getImagenById/{id}', [C_ReporteController::class, 'getImagenReportesById']);
 //delete all
 Route::delete('deleteAll', [C_ReporteController::class, 'deleteAll']);
