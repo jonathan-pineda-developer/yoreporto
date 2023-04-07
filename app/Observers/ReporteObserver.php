227e111fc3a1692338d905bc598fbbdeb25fad77
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-04-07 14:10:00 -0600

    agregando para que guarde la justificacion al rechazar reporte

--- app/Observers/ReporteObserver.php
+++ app/Observers/ReporteObserver.php
@@ -15,12 +15,15 @@ class ReporteObserver
      * @param  \App\Models\C_Reporte  $reporte
      * @return void
      */
+    public function updated1(C_Reporte $reporte,$request)
+    {
+        $this->logOperation('Rechazado', $reporte, $request);
+    }
     public function updated(C_Reporte $reporte)
     {
-        $this->logOperation('updated', $reporte);
-
+        $this->logOperation1('Aceptado', $reporte);
     }
-    private function logOperation(string $operation, C_Reporte $reporte)
+    public function logOperation(string $operation, C_Reporte $reporte, $request)
     {
         // Traer el nombre completo del UTE que realizó la operación y está logueado
         $user_id = auth()->user()->id;
@@ -32,9 +35,41 @@ class ReporteObserver
         $modified_at = $reporte->updated_at;
     
         // Determinar el valor de la operación en función del estado del reporte
-        $operation = $reporte->estado === 'Aceptado' || $reporte->estado === 'Rechazado'
-        ? $reporte->estado
-        : 'Modificado';
+        if($reporte->estado === 'Aceptado'){
+            $operation = 'Aceptado';
+        }elseif($reporte->estado === 'Rechazado'){
+            $operation = 'Rechazado';
+        }else{
+            $operation = 'Finalizado';
+        }
+    
+        DB::table('TB_Bitacora')->insert([
+            'operation' => $operation,
+            'justificacion' => $request,
+            'ute' => $ute,
+            'reporte_id' => $reporte_id,
+            'modified_at' => $modified_at,
+        ]);
+    }
+    public function logOperation1(string $operation, C_Reporte $reporte)
+    {
+        // Traer el nombre completo del UTE que realizó la operación y está logueado
+        $user_id = auth()->user()->id;
+        $ute = User::select(DB::raw("CONCAT(nombre, ' ', apellidos) AS nombre_completo"))
+                   ->where('id', $user_id)
+                   ->where('rol', 'UTE')
+                   ->value('nombre_completo');
+        $reporte_id = $reporte->id;
+        $modified_at = $reporte->updated_at;
+    
+        // Determinar el valor de la operación en función del estado del reporte
+        if($reporte->estado === 'Aceptado'){
+            $operation = 'Aceptado';
+        }elseif($reporte->estado === 'Rechazado'){
+            $operation = 'Rechazado';
+        }else{
+            $operation = 'Finalizado';
+        }
     
         DB::table('TB_Bitacora')->insert([
             'operation' => $operation,
