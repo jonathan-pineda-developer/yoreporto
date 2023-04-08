15ba7fbfd415e68838d8013a8ec599b5d551ea8b
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-04-08 12:56:54 -0600

    dejo el observer comentado por cualquier cosa

--- app/Observers/ReporteObserver.php
+++ app/Observers/ReporteObserver.php
@@ -2,8 +2,9 @@
 
 namespace App\Observers;
 use App\Models\C_Reporte;
-use App\Models\C_Categoria;
+use App\Models\C_Bitacora;
 use App\Models\User;
+use App\Mail\RechazoReporte;
 use Illuminate\Support\Facades\DB;
 
 
@@ -15,43 +16,16 @@ class ReporteObserver
      * @param  \App\Models\C_Reporte  $reporte
      * @return void
      */
-    public function updated1(C_Reporte $reporte,$request)
+
+  /*  public function updated(C_Reporte $reporte, $motivo=null)
     {
-        $this->logOperation('Rechazado', $reporte, $request);
+        $this->logOperation('updated',$reporte, $motivo);
     }
-    public function updated(C_Reporte $reporte)
+    /*public function updated1(C_Reporte $reporte)
     {
-        $this->logOperation1('Aceptado', $reporte);
+        $this->logOperation1($reporte);
     }
-    public function logOperation(string $operation, C_Reporte $reporte, $request)
-    {
-        // Traer el nombre completo del UTE que realizó la operación y está logueado
-        $user_id = auth()->user()->id;
-        $ute = User::select(DB::raw("CONCAT(nombre, ' ', apellidos) AS nombre_completo"))
-                   ->where('id', $user_id)
-                   ->where('rol', 'UTE')
-                   ->value('nombre_completo');
-        $reporte_id = $reporte->id;
-        $modified_at = $reporte->updated_at;
-    
-        // Determinar el valor de la operación en función del estado del reporte
-        if($reporte->estado === 'Aceptado'){
-            $operation = 'Aceptado';
-        }elseif($reporte->estado === 'Rechazado'){
-            $operation = 'Rechazado';
-        }else{
-            $operation = 'Finalizado';
-        }
-    
-        DB::table('TB_Bitacora')->insert([
-            'operation' => $operation,
-            'justificacion' => $request,
-            'ute' => $ute,
-            'reporte_id' => $reporte_id,
-            'modified_at' => $modified_at,
-        ]);
-    }
-    public function logOperation1(string $operation, C_Reporte $reporte)
+    public function logOperation(string $operation,C_Reporte $reporte, $motivo)
     {
         // Traer el nombre completo del UTE que realizó la operación y está logueado
         $user_id = auth()->user()->id;
@@ -67,8 +41,40 @@ class ReporteObserver
             $operation = 'Aceptado';
         }elseif($reporte->estado === 'Rechazado'){
             $operation = 'Rechazado';
-        }else{
+        }elseif($reporte->estado === 'Finalizado'){
             $operation = 'Finalizado';
+        }else{
+            $operation = 'Cambio de categoría';
+        }
+    
+        DB::table('TB_Bitacora')->insert([
+            'operation' => $operation,
+            'justificacion' => $motivo,
+            'ute' => $ute,
+            'reporte_id' => $reporte_id,
+            'modified_at' => $modified_at,
+        ]);
+    }
+   /* public function logOperation1(C_Reporte $reporte)
+    {
+        // Traer el nombre completo del UTE que realizó la operación y está logueado
+        $user_id = auth()->user()->id;
+        $ute = User::select(DB::raw("CONCAT(nombre, ' ', apellidos) AS nombre_completo"))
+                   ->where('id', $user_id)
+                   ->where('rol', 'UTE')
+                   ->value('nombre_completo');
+        $reporte_id = $reporte->id;
+        $modified_at = $reporte->updated_at;
+    
+        // Determinar el valor de la operación en función del estado del reporte
+        if($reporte->estado === 'Aceptado'){
+            $operation = 'Aceptado';
+        }elseif($reporte->estado === 'Rechazado'){
+            $operation = 'Rechazado';
+        }elseif($reporte->estado === 'Finalizado'){
+            $operation = 'Finalizado';
+        }else{
+            $operation = 'Cambio de categoría';
         }
     
         DB::table('TB_Bitacora')->insert([
@@ -77,6 +83,6 @@ class ReporteObserver
             'reporte_id' => $reporte_id,
             'modified_at' => $modified_at,
         ]);
-    }
+    }*/
     
 }
