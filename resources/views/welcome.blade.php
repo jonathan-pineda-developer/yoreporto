a60354b594595705c09e9d239b280cdf923d0d68
JonathanPA12 <jonathan.pineda.aleman@est.una.ac.cr>
2023-06-05 23:04:44 -0600

    agrego nuevos archivos disc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,6 +59,6 @@
   <div class="page-loading">
     <div class="spinner-border text-danger loading-icon" role="status"></div>
   </div>
-<script src="runtime.f070ea0451aa6974.js" type="module"></script><script src="polyfills.c2d0e72ba3ab7af1.js" type="module"></script><script src="scripts.8a4cfb6a34d601d2.js" defer></script><script src="main.d2275470d7de2ec1.js" type="module"></script>
+<script src="runtime.68fa06b2e63b0139.js" type="module"></script><script src="polyfills.c2d0e72ba3ab7af1.js" type="module"></script><script src="scripts.8a4cfb6a34d601d2.js" defer></script><script src="main.503b6184c0e612fc.js" type="module"></script>
 
 </body></html>
